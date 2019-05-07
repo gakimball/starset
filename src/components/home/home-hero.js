446ea63d78abeef89e7858ae74885b427bb8cbfa
Geoff Kimball <geoff@geoffkimball.com>
2019-05-06 22:32:55 -0700

    Mess around a bit with typography

---
+++ src/components/home/home-hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './home-hero.module.css';
+
+const HomeHero = () => (
+  <>
+    <h1 className={s.title}>Sound design for the future.</h1>
+    <p className={s.subtitle}>We make beautiful sounds for commercial and interactive projects. _</p>
+  </>
+);
+
+export default HomeHero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout/layout';
 import Container from '../components/container/container';
+import HomeHero from '../components/home/home-hero';
 import SolarSystem from '../components/solar-system/solar-system';
 import Row from '../components/grid/row';
 import Column from '../components/grid/column';
@@ -8,7 +9,7 @@ import Column from '../components/grid/column';
 export default () => (
   <Layout>
     <Container>
-      Hi I&apos;m website
+      <HomeHero/>
       <SolarSystem/>
       <Row>
         <Column width={6}>Left</Column>
